7e344e124897a923dfd1e39fa2ad90b9052cb362
Rocky.Zhang <122086517@qq.com>
2019-09-02 12:50:29 +0800

    移除无用变量

--- lib/config/app_store.dart
+++ lib/config/app_store.dart
@@ -1,7 +1,5 @@
 import 'dart:io';
 
-import 'dart:ui';
-
 /// 是否在审核期间
 bool get appStoreReview {
   return Platform.isIOS && DateTime.now().isBefore(DateTime(2019, 9, 3, 9, 30));
@@ -17,8 +15,5 @@ String replaceAndroid(String str) {
   return str;
 }
 
-const String flutterIconUrl = 'assets/images/splash_flutter.png';
-const String funIconUrl = 'assets/images/splash_fun.png';
-const String avatarIconUrl = 'assets/images/avatar.png';
-
-const String appleBannerUrl = 'https://www.wanandroid.com/blogimgs/90c6cc12-742e-4c9f-b318-b912f163b8d0.png';
+const String appleBannerUrl =
+    'https://www.wanandroid.com/blogimgs/90c6cc12-742e-4c9f-b318-b912f163b8d0.png';
