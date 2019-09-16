b8fdff2bada3b860c4d6cadd74d625152ea69410
Rocky.Zhang <122086517@qq.com>
2019-09-16 10:43:29 +0800

    重构http初始化方式,使扩展性更强

--- lib/config/storage_manager.dart
+++ lib/config/storage_manager.dart
@@ -19,13 +19,12 @@ class StorageManager {
   ///
   /// 由于是同步操作会导致阻塞,所以应尽量减少存储容量
   static init() async {
-    debugPrint('StorageManager--init');
     // async 异步操作
+    
     // sync 同步操作
     temporaryDirectory = await getTemporaryDirectory();
     sharedPreferences = await SharedPreferences.getInstance();
     localStorage = LocalStorage('LocalStorage');
     await localStorage.ready;
-    debugPrint('StorageManager--end');
   }
 }
