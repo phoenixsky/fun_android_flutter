a1fa1b5e7ac48cf6f94fcdb4500d8cc2139c1d13
Rocky.Zhang <122086517@qq.com>
2019-10-14 12:50:28 +0800

    闪屏页根据明暗加入透明度

--- lib/ui/page/splash.dart
+++ lib/ui/page/splash.dart
@@ -1,4 +1,5 @@
 import 'dart:async';
+import 'dart:ui';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_swiper/flutter_swiper.dart';
@@ -55,6 +56,9 @@ class _SplashPageState extends State<SplashPage> with TickerProviderStateMixin {
         onWillPop: () => Future.value(false),
         child: Stack(fit: StackFit.expand, children: <Widget>[
           Image.asset(ImageHelper.wrapAssets('splash_bg.png'),
+              colorBlendMode: BlendMode.srcOver,
+              color: Colors.black.withOpacity(
+                  Theme.of(context).brightness == Brightness.light ? 0 : 0.65),
               fit: BoxFit.fill),
           AnimatedFlutterLogo(
             animation: _animation,
