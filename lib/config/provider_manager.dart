ee756ff8a776b592af9686bd42b4c4404c255400
liyujiang <1032694760@qq.com>
2019-12-17 16:02:03 +0800

    fix out-of-date api

--- lib/config/provider_manager.dart
+++ lib/config/provider_manager.dart
@@ -13,13 +13,13 @@ List<SingleChildCloneableWidget> providers = [
 /// 独立的model
 List<SingleChildCloneableWidget> independentServices = [
   ChangeNotifierProvider<ThemeModel>(
-    builder: (context) => ThemeModel(),
+    create: (context) => ThemeModel(),
   ),
   ChangeNotifierProvider<LocaleModel>(
-    builder: (context) => LocaleModel(),
+    create: (context) => LocaleModel(),
   ),
   ChangeNotifierProvider<GlobalFavouriteStateModel>(
-    builder: (context) => GlobalFavouriteStateModel(),
+    create: (context) => GlobalFavouriteStateModel(),
   )
 ];
 
@@ -28,7 +28,8 @@ List<SingleChildCloneableWidget> independentServices = [
 /// UserModel依赖globalFavouriteStateModel
 List<SingleChildCloneableWidget> dependentServices = [
   ChangeNotifierProxyProvider<GlobalFavouriteStateModel, UserModel>(
-    builder: (context, globalFavouriteStateModel, userModel) =>
+    create: null,
+    update: (context, globalFavouriteStateModel, userModel) =>
         userModel ??
         UserModel(globalFavouriteStateModel: globalFavouriteStateModel),
   )
