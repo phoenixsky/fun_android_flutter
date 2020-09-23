ab86bf0f023db01e1b06e7b027306265cb406c42
weikaiyun <weikaiyun@pwrd.com>
2020-09-23 14:31:29 +0800

    删除无用代码

--- lib/config/router_manger.dart
+++ lib/config/router_manger.dart
@@ -83,29 +83,3 @@ class Router {
     }
   }
 }
-
-/// Pop路由
-class PopRoute extends PopupRoute {
-  final Duration _duration = Duration(milliseconds: 300);
-  Widget child;
-
-  PopRoute({@required this.child});
-
-  @override
-  Color get barrierColor => null;
-
-  @override
-  bool get barrierDismissible => true;
-
-  @override
-  String get barrierLabel => null;
-
-  @override
-  Widget buildPage(BuildContext context, Animation<double> animation,
-      Animation<double> secondaryAnimation) {
-    return child;
-  }
-
-  @override
-  Duration get transitionDuration => _duration;
-}
