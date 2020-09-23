49dbc50d86c3634030bc4a11fece26641c3ffb4b
weikaiyun <weikaiyun@pwrd.com>
2020-09-23 17:12:21 +0800

    删除无用代码

--- lib/ui/page/splash.dart
+++ lib/ui/page/splash.dart
@@ -2,7 +2,6 @@ import 'dart:async';
 import 'dart:ui';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
-import 'package:flutter_swiper/flutter_swiper.dart';
 
 import 'package:fun_android/config/router_manger.dart';
 import 'package:fun_android/config/resource_mananger.dart';
@@ -179,55 +178,4 @@ class AnimatedAndroidLogo extends AnimatedWidget {
 
 void nextPage(context) {
   Navigator.of(context).pushReplacementNamed(RouteName.tab);
-}
-
-class GuidePage extends StatefulWidget {
-  static const List<String> images = <String>[
-    'guide_page_1.png',
-    'guide_page_2.png',
-    'guide_page_3.png',
-    'guide_page_4.png'
-  ];
-
-  @override
-  _GuidePageState createState() => _GuidePageState();
-}
-
-class _GuidePageState extends State<GuidePage> {
-  int curIndex = 0;
-
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-        body: Container(
-      color: Theme.of(context).primaryColor,
-      child: Stack(
-        alignment: Alignment(0, 0.87),
-        children: <Widget>[
-          Swiper(
-              itemBuilder: (ctx, index) => Image.asset(
-                    'assets/images/${GuidePage.images[index]}',
-                    fit: BoxFit.fill,
-                  ),
-              itemCount: GuidePage.images.length,
-              loop: false,
-              onIndexChanged: (index) {
-                setState(() {
-                  curIndex = index;
-                });
-              }),
-          Offstage(
-            offstage: curIndex != GuidePage.images.length - 1,
-            child: CupertinoButton(
-              color: Theme.of(context).primaryColorDark,
-              child: Text('点我开始'),
-              onPressed: () {
-                nextPage(context);
-              },
-            ),
-          )
-        ],
-      ),
-    ));
-  }
 }
