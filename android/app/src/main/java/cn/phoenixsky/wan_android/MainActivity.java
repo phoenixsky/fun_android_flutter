9c52f391fc971fed1abc1731c279086ee3afbe99
phoenixsky <moran.fc@gmail.com>
2021-04-11 21:59:28 +0800

    migrated to the android embedding v2

--- android/app/src/main/java/cn/phoenixsky/wan_android/MainActivity.java
+++ android/app/src/main/java/cn/phoenixsky/wan_android/MainActivity.java
@@ -1,13 +1,6 @@
 package cn.phoenixsky.wan_android;
 
-import android.os.Bundle;
-import io.flutter.app.FlutterActivity;
-import io.flutter.plugins.GeneratedPluginRegistrant;
+import io.flutter.embedding.android.FlutterActivity;
 
 public class MainActivity extends FlutterActivity {
-  @Override
-  protected void onCreate(Bundle savedInstanceState) {
-    super.onCreate(savedInstanceState);
-    GeneratedPluginRegistrant.registerWith(this);
-  }
 }
