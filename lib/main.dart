2277c2a642e0fbcd8120b0eb829e709ccf9eab5f
phoenixsky <moran.fc@gmail.com>
2021-04-25 11:24:15 +0800

    feat(translation): 国际化支持

--- lib/main.dart
+++ lib/main.dart
@@ -2,14 +2,17 @@ import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 import 'package:funflutter_wandroid/ui/router/app_pages.dart';
 
+import 'dart:ui' as ui;
+
 import 'package:get/get.dart';
+
+import 'constants/app_translations.dart';
 
 void main() {
   // Android状态栏透明 splash为白色,所以调整状态栏文字为黑色
   SystemChrome.setSystemUIOverlayStyle(SystemUiOverlayStyle(
       statusBarColor: Colors.transparent,
       statusBarBrightness: Brightness.light));
-
   runApp(MyApp());
 }
 
@@ -17,11 +20,16 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return GetMaterialApp(
-      debugShowCheckedModeBanner: false,
-      initialRoute: AppPages.INITIAL,
-      // theme: appThemeData,
-      defaultTransition: Transition.fade,
-      getPages: AppPages.pages,
-    );
+        debugShowCheckedModeBanner: false,
+        // theme: appThemeData,
+        initialRoute: AppPages.INITIAL,
+        defaultTransition: Transition.fade,
+        getPages: AppPages.pages,
+        // locale
+        translations: AppTranslations(),
+        // 系统语言
+        locale: ui.window.locale,
+        // 添加一个回调语言选项，以备上面指定的语言翻译不存在
+        fallbackLocale: Locale('en', 'US'));
   }
 }
